f318b99bea71b58541b37b3b7b37aa780daf7520
OptimisticRservoir <43356080+OptimisticReservoir@users.noreply.github.com>
2018-10-15 17:56:49 -0700

    Solved p4 and updated template for user input

--- pe/p4.py
+++ pe/p4.py
@@ -18,5 +18,89 @@
 # and it will avoid unnecessary calculations, e.g. 909 x 101
 
 
+import math
+import sys
+
+def main(args):
+    maxDigits = 0
+    palindrome = ProductPalindrome()
+    if(len(args)>1 and castNumber(args[1])):
+        # Use passed arguments
+        maxDigits = int(float(args[1]))
+    else:
+        # Use default parameters
+        maxDigits = 3
+    palindrome = findMaxPalindrome(maxDigits)
+    print("Largest palindrome with " + str(maxDigits) + " is " +
+            str(palindrome.value) + " from " + str(palindrome.x) + 
+            " and " + str(palindrome.y) + " and it is " + 
+            ("valid." if palindrome.valid else "not valid."))
+
+def findMaxPalindrome(digits):
+    digits = int(castNumber(digits))
+    if not digits:
+        digits = 3 # use 3 as default if invalid digit given.
+    maxNum = int("9"*digits) # all 9s is max value
+    maxIndex = 2 * maxNum # index = sum of the two numbers to be multipled.
+    i = maxIndex
+    x = maxNum
+    y = maxNum
+    solved = False
+    # I'm postulating that this fill find the maximum palindrome first
+    # if not, I'll need to add all palindromes found to a list and then
+    # find max later.
+    while(i > 0):
+        x = maxNum
+        y = i - x
+        # x + y = i
+        while(y <= maxNum):
+            if isPalindrome(x*y):
+                solved = True
+                break
+            x -= 1
+            y += 1
+        if solved:
+            break
+        i -= 1
+    return ProductPalindrome(x,y)
+
+class ProductPalindrome:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+        self.value = x*y
+        self.valid = isPalindrome(self.value)
+
+def isPalindrome(s):
+    s = str(s)
+    i = 0
+    while(i < len(s)/2):
+        if s[i] != s[-(i+1)]: # 0 is first, -1 is last.
+            return False
+        i += 1
+    return True
+
+def castNumber(n):
+    if is_intstring(n):
+        return int(n)
+    elif is_floatstring(n):
+        return float(n)
+    else:
+        return None
+
+def is_floatstring(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+def is_intstring(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
 
 
+main(sys.argv)
