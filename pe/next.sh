46dd19f045c35bf0c89d207e0bb1a4853f004281
OptimisticRservoir <43356080+OptimisticReservoir@users.noreply.github.com>
2018-10-18 11:18:12 -0700

    TODO: automate cp template

---
+++ pe/next.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# To DO:
+# Make a script that will find the last problem euler file
+# then make the next
+
+# So far, I have ls p* | grep [0-9]* | tail -1
+
+# the problem is that it sorts 1, 10, 11, 2, 3, ...
+
+# I'll need to first rename and re-git the p1-9/py files
+
+# then after that, I should pull that file name's number,
+# increment and then cp the template to the new number.
