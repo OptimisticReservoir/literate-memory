09ba4fb3786e28b9449654eb70d0789dc08d6452
OptimisticRservoir <43356080+OptimisticReservoir@users.noreply.github.com>
2018-10-20 10:49:38 -0700

    Created a script to make next PE file

--- pe/next.sh
+++ pe/next.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
-# To DO:
-# Make a script that will find the last problem euler file
-# then make the next
-
-# So far, I have ls p* | grep [0-9]* | tail -1
-
-# the problem is that it sorts 1, 10, 11, 2, 3, ...
-
-# I'll need to first rename and re-git the p1-9/py files
-
-# then after that, I should pull that file name's number,
-# increment and then cp the template to the new number.
+echo "Making a new Project Euler file."
+last_file=$(ls p* | grep [0-9]* | tail -1)
+echo "Last file was "$last_file
+next_num=$(expr $(echo $last_file|grep -o [0-9]*) + 1)
+next_file="p"$next_num".py"
+echo "Next file is "$next_file
+cp ./template.py ./$next_file
+git add ./$next_file
+git commit -m "Created $next_file"
+echo $next_file" created and added to git."
