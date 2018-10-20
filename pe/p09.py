e327c38776ab4e696a596d8379948172b655bcdb
OptimisticRservoir <43356080+OptimisticReservoir@users.noreply.github.com>
2018-10-20 12:48:16 -0700

    Added another solution from forums

--- pe/p09.py
+++ pe/p09.py
@@ -25,16 +25,55 @@
 # So we can set c**2 - 2x**2 = 2y**2 and solve for y.
 
 import sys
+import time
 
 def main(args):
     index = 0
+    result = [0,0,0]
     if(len(args)>1 and cast_number(args[1])):
         index = int(float(args[1]))
     else:
         index = 1000
+    t_start = time.time()
+    result = find_pythagorean_triple(index)
+    print_pythagorean_triple(result,index)
+    t_1 = time.time()
+    result = other_find_pythagorean_triple(index)
+    print_pythagorean_triple(result,index)
+    t_2 = time.time()
+    print(f"My solution took {t_1-t_start}"
+      + f" and other solution took {t_2-t_1}")
+
+
+def other_find_pythagorean_triple(i=1000):
+    for triple in triple_generator(i):
+        if sum(triple) == i:
+            #print("(\{x\}/\{y\}) =>"+f" {triple}, {sum(triple)}")
+            return triple
+    return [0,0,0]
+
+def triple_generator(i=1000):
+    # Generates pythagorean triples
+    limit = int((i/2)**0.5)+5
+    for n in range(limit):
+        for m in range(n, limit):
+            triple = [m**2 - n**2, 2*m*n, m**2 + n**2]
+            if sum(triple) <= i:
+                yield [int(m**2 - n**2), int(2*m*n), int(m**2 + n**2)]
+
+def print_pythagorean_triple(p_triple,index):
+    print(f"A pythagorean triple for index of {index} is:")
+    print(f"a: {p_triple[0]}"
+      + f" b: {p_triple[1]}"
+      + f" c: {p_triple[2]}")
+    print(f"The sum is {sum(p_triple)}"
+      + f" which is {'valid' if index==sum(p_triple) else 'not valid'}")
+    print(f"The product is {list_product(p_triple)}")
+
+def find_pythagorean_triple(i=1000):
     pythagoreanTriple = [0,0,0]
-    for c in range(int(index/2 - 0.1),1,-1):
-        x = (index - c)/2
+    for c in range(int(i/2 - 0.1),1,-1):
+        x = (i - c)/2
         y2 = ((c**2 - 2*x**2)/2)
         if y2 < 0:
             #can't square root a negative number here.
@@ -43,13 +82,9 @@ def main(args):
         #print(f"y={y} c={c} c%2/2={(c%2)/2}")
         if y%1 == (c%2)/2:
             # y = integer if c is even, or int + 0.5 if odd.
-            pythagoreanTriple = [x-y, x+y, c]
+            pythagoreanTriple = [int(x-y), int(x+y), int(c)]
             break
-    print(f"A pythagorean triple for index of {index} is:")
-    print(f"a: {pythagoreanTriple[0]}"
-      + f" b: {pythagoreanTriple[1]}"
-      + f" c: {pythagoreanTriple[2]}")
-    print(f"The product is {list_product(pythagoreanTriple)}")
+    return pythagoreanTriple
 
 def list_product(list):
     val = 1
