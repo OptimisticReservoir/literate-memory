fa34ce992dbef7b8ff8bb06c7646ce6e02297503
OptimisticRservoir <43356080+OptimisticReservoir@users.noreply.github.com>
2018-10-20 10:48:12 -0700

    Created p11.py

---
+++ pe/p11.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+
+
+import sys
+
+def main(args):
+    if(len(args)>1):
+        # Use passed arguments
+    else:
+        # Use default parameters
+
+def cast_number(n):
+    if is_intstring(n):
+        return int(n)
+    elif is_floatstring(n):
+        return float(n)
+    else:
+        return None
+
+def is_floatstring(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+def is_intstring(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+
+main(sys.argv)
