108a4e2bd68429c2bd51224f8cb430bd08452a81
OptimisticRservoir <43356080+OptimisticReservoir@users.noreply.github.com>
2018-10-21 23:08:20 -0700

    fixed bug caused by a "0" argument

--- pe/template.py
+++ pe/template.py
@@ -9,7 +9,7 @@ from functions import cast_number
 
 def main(args):
     # arg_f(argument_list, index, validity_function, default_value)
-    arg_f = lambda a, i, f, d: f(a[i]) if len(a)>i and f(a[i]) else d
+    arg_f = lambda a, i, f, d: f(a[i]) if len(a)>i and f(a[i]) != None else d
     ARGUMENT1 = arg_f(args,1,cast_number,"default 1")
     ARGUMENT2 = arg_f(args,2,cast_number,"default 2")
     ARGUMENT3 = arg_f(args,3,cast_number,"default 3")
