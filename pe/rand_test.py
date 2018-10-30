8ac67f37d2a43a40d7c8dc2eecfdd248daa812f0
OptimisticRservoir <43356080+OptimisticReservoir@users.noreply.github.com>
2018-10-30 02:42:37 -0700

    Added static function example.

--- pe/rand_test.py
+++ pe/rand_test.py
@@ -8,6 +8,12 @@ import sys
 from functions import cast_number
 from my_random import MyRNG
 
+def static_test():
+    if "x" not in static_test.__dict__:
+        static_test.x = 0
+    static_test.x += 1
+    print(static_test.x)
+
 def main(args):
     # arg_f(argument_list, index, validity_function, default_value)
     arg_f = lambda a, i, f, d: f(a[i]) if len(a)>i and f(a[i]) != None else d
