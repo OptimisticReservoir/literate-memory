2334c0754156c51a56f846d2af05a345c7117eb3
OptimisticRservoir <43356080+OptimisticReservoir@users.noreply.github.com>
2018-10-29 23:11:51 -0700

    Improved argument support to print the prime list.

--- pe/p07.py
+++ pe/p07.py
@@ -6,16 +6,19 @@
 
 import sys
 from functions import ordinal
+from functions import cast_number
+from functions import get_boolstring
+#from functions import is_boolstring
 from functions import generate_n_primes
 
 def main(args):
-    nthPrime = 0
-    if(len(args)>1 and cast_number(args[1])):
-        nthPrime = int(float(args[1]))
-    else:
-        nthPrime = 10001
+    # arg_f(argument_list, index, validity_function, default_value)
+    arg_f = lambda a, i, f, d: f(a[i]) if len(a)>i and f(a[i]) != None else d
+    nthPrime = arg_f(args,1,cast_number,10001)
+    # get_boolstring is fine, because if it gets False, that's default anyway.
+    print_results = arg_f(args,2,get_boolstring,False) 
     primes = generate_n_primes(nthPrime)
-    if nthPrime <= 100:
+    if print_results:
         for p in primes:
             print(p)
     print("The " + str(nthPrime) + ordinal(nthPrime)
