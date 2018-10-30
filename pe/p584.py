71428ddaeb12c25ff8264b9012f2886fb72bec90
OptimisticRservoir <43356080+OptimisticReservoir@users.noreply.github.com>
2018-10-30 02:43:38 -0700

    Updated to use my RNG new RNG code using my class, file I/O now uses with, added comment stating the status of this problem.

--- pe/p584.py
+++ pe/p584.py
@@ -15,9 +15,21 @@
 # 7 days of each other: 365 day year
 # Answer: Find it.
 
+# This method attempts to use the monte carlo method
+# random random random and measure.
+# It doesn't work.
+# It wants answers with 8 decimal places.
+# I can only get 2-3 with this method.
+# Random fluctuations are painful.
+# And my answers run ~1-2% high usually.
+# Maybe random isn't random enough...
+
+# I'll need to make a new solution that is analytical.
+
 import sys
 import random
 
+from my_random import MyRNG
 from functions import cast_number
 
 def main(args):
@@ -33,6 +45,9 @@ def main(args):
     match_list = []
     counter = 0
     lengths = []
+    print(f"Testing scenario needing {matches} matches,{nl}"
+    + f"within {tolerance} day period,{nl}"
+    + f"in a year {year_len} days long.")
     for i in b_days_generator:
         b_days.append(i)
         current_list = check_b_day_match(b_days,matches,tolerance,year_len)
@@ -40,18 +55,16 @@ def main(args):
         if current_list:
             match_list.append(b_days.copy())
             b_days.clear()
-        if counter>1000000: break
+        if counter>330000/3: break
+        # 330000 = ~30 seconds on my computer
     for i in match_list:
         #print(i)
         lengths.append(len(i))
-    print(f"Testing scenario needing {matches} matches,{nl}"
-    + f"within {tolerance} day period,{nl}"
-    + f"in a year {year_len} days long.")
     print(f"After {len(lengths)} trials,"
     + f" the result is {sum(lengths)/len(lengths)}.")
-    fd = open("results_584.csv","a")
     run_conditions = f"{matches}_{tolerance}_{year_len}"
-    fd.write(f"{run_conditions},{sum(lengths)},{len(lengths)}{nl}")
+    with open("results_584.csv","a") as fd:
+        fd.write(f"{run_conditions},{sum(lengths)},{len(lengths)}{nl}")
 
 
 def is_within_x_wrap(a,b,x=0,mod=0):
@@ -82,11 +95,20 @@ def check_b_day_match(list,matches,tolerance,year_len):
 
 def generate_birthdays(year_len):
     USE_SYS = False
-    if USE_SYS:
-        r = random.SystemRandom()
+    USE_MINE = True
+    # Below is for making r a static variable.
+    # However, this shouldn't be needed since we are using yield.
+    #if USE_MINE and "r" not in generate_birthdays.__dict__:
+    #    r = MyRNG()
+    if USE_MINE:
+        r = MyRNG()
+        while(1):
+            yield r.rand_range(year_len)
+    elif USE_SYS:
+        sys_r = random.SystemRandom()
         while(1):
             # yield r.randrange(year_len)
-            yield r.randrange(year_len)
+            yield sys_r.randrange(year_len)
     else:
         random.seed()
         while(1):
